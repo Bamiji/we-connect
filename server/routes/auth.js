8fb44cf16a964959ce4195a001b2c96a90219726
Banji Afolabi <banji@banjislab.com>
2018-03-12 20:48:44 +0100

    Adhere to airbnb eslint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,24 +1,22 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
-  const name = req.body.name;
+  const { name } = req.body;
 
   res.send(`User ${name} has been registered.`);
 });
 
 router.post('/login', (req, res) => {
-  user = {name: 'banji', password: 'banji'};
-  const name = req.body.name;
-  const password = req.body.password;
+  const user = { name: 'banji', password: 'banji' };
+  const { name, password } = req.body;
 
-  if (name == user.name && password == user.password) {
+  if (name === user.name && password === user.password) {
     res.send(`User ${name} has been logged in.`);
+  } else {
+    res.send('Login failed.');
   }
-  else{
-    res.send('Login failed.')
-  }
-
 });
 
 module.exports = router;
